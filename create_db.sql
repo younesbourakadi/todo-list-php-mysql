ace72f79a004a9b9e6fea18c828b1734a47ee2c7
Younes Bourakadi <younes.bourakadi@gmail.com>
2023-06-27 16:37:24 +0200

    Fix create_date type

--- create_db.sql
+++ create_db.sql
@@ -6,14 +6,14 @@ CREATE TABLE client (
   id_client INT PRIMARY KEY AUTO_INCREMENT,
   lastname_client VARCHAR(255),
   firstname_client VARCHAR(255),
-  email_client VARCHAR(255),
+  email_client VARCHAR(255)
   password_client VARCHAR(255)
 );
 
 CREATE TABLE task (
   id_task INT PRIMARY KEY AUTO_INCREMENT,
   description_task VARCHAR(255),
-  date_creation DATE,
+  date_creation DATETIME,
   client_id INT,
   FOREIGN KEY (client_id) REFERENCES client(id)
 );
