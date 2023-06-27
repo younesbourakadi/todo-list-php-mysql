938681f4fd06a074f515ae5965fe362d05c3de76
VictorPeller1 <victorpellerin14@gmail.com>
2023-06-27 16:25:20 +0200

    website done

---
+++ index.php
@@ -0,0 +1,39 @@
+<? phpinfo ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+  <body>
+
+    
+    
+    <div class="container">
+
+      <div class="div-flex">
+      <input type"note" name="note"></input>
+        <button class="btn-form"></button>
+        </div>
+      <div class="div-flex">
+      <ul>
+        <li>this is a li</li><button class="btn-li"></button>
+      </ul>
+      </div>
+
+
+    </div>
+
+
+
+  
+    
+    
+    
+    
+      
+    
+</body>
+</html>
